4a2c41bf96b57354ccc322f87d79f302f3b9f690
Maëlys Deschaux <maelys.dcb@gmail.com>
2023-01-19 16:51:44 +0100

    Resolve database issues, status buttons in process

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// DATABASE
+// VARIABLES
+let toDoContainer = document.querySelector(".thisMonth");
 let db = [
   {
     text: "",
@@ -11,11 +12,12 @@ let db = [
 // ADDING TASKS
 function addToDo() {
   let inputElements = document.querySelectorAll(".task");
+  let btn = document.querySelectorAll(".btn");
   let task = {
     text: inputElements[0].value,
-    status: inputElements[1].value,
-    date: inputElements[2].value,
-    time: inputElements[3].value,
+    status: btn[0].textContent,
+    date: inputElements[1].value,
+    time: inputElements[2].value,
   };
 
   if (task.text == "") {
@@ -27,11 +29,9 @@ function addToDo() {
 }
 
 // DISPLAY VALUES
-let toDoContainer = document.querySelector(".thisMonth");
-
 function display() {
   toDoContainer.innerHTML = "";
-  for (let i = 0; i < db.length; i++) {
+  for (let i = 1; i < db.length; i++) {
     let d = document.createElement("div");
     d.setAttribute("style", "padding:1em 0;");
     d.append(task(i), state(i), date(i), time(i), remove(i));
@@ -47,9 +47,13 @@ function task(i) {
 }
 
 function state(i) {
-  let input = document.createElement("input");
-  input.value = db[i].status;
-  return input;
+  let btn = document.createElement("button");
+  btn.classList.add("test");
+  btn.textContent = db[i].status;
+  btn.addEventListener("click", function () {
+    btnState(btn, "visible");
+  });
+  return btn;
 }
 
 function date(i) {
@@ -69,9 +73,9 @@ function time(i) {
 // REMOVE
 function remove(i) {
   let btn = document.createElement("button");
+  btn.className = "addRemove";
   let btnText = document.createTextNode("-");
   btn.appendChild(btnText);
-  //   btn.innerHTML = "-";
   btn.setAttribute("style", "background-color:#FE4A49;");
   btn.addEventListener("click", () => {
     console.log(i);
@@ -81,4 +85,32 @@ function remove(i) {
   return btn;
 }
 
-// MODIFY
+// STATUS BUTTON
+let currentButton;
+let btnStatus = document.querySelector("button#state");
+let tooltipHide = document.querySelector(".tooltip");
+tooltipHide.classList.toggle("hide");
+
+function btnState(v, state) {
+  if (state == "visible") {
+    currentButton = v;
+    tooltipHide.style.visibility = "visible";
+    tooltipHide.classList.toggle("hide");
+  } else if (state == "done") {
+    currentButton.textContent = `Done`;
+    currentButton.setAttribute("style", "background-color: rgb(84, 199, 84);");
+    tooltipHide.classList.toggle("hide");
+  } else if (state == "inProcess") {
+    currentButton.textContent = `In process`;
+    currentButton.setAttribute("style", "background-color: rgb(255, 180, 42);");
+    tooltipHide.classList.toggle("hide");
+  } else if (state == "blocked") {
+    currentButton.textContent = `Blocked`;
+    currentButton.setAttribute("style", "background-color: rgb(255, 62, 62);");
+    tooltipHide.classList.toggle("hide");
+  } else if (state == "grey") {
+    currentButton.textContent = `Status`;
+    currentButton.setAttribute("style", "background-color:grey");
+    tooltipHide.classList.toggle("hide");
+  }
+}
